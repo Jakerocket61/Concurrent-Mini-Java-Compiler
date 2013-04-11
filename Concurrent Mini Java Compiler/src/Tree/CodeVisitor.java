3b88090a00921b821944a4add785b8068aad5c6a
Paul <paul.kaefer@marquette.edu>
2013-04-10 21:52:21 -0500

    coded Tree/CodeVisitor.java just like Dr. Brylow's example

--- Concurrent Mini Java Compiler/src/Tree/CodeVisitor.java
+++ Concurrent Mini Java Compiler/src/Tree/CodeVisitor.java
@@ -1,5 +1,18 @@
 package Tree;
 
-public class CodeVisitor {
-
+public interface CodeVisitor {
+	public void visit(SEQ n);
+	public void visit(LABEL n);
+	public void visit(JUMP n);
+	public void visit(CJUMP n);
+	public void visit(MOVE n);
+	public void visit(EXP n);
+	
+	public Temp.Temp visit(BINOP n);
+	public Temp.Temp visit(MEM n);
+	public Temp.Temp visit(TEMP n);
+	public Temp.Temp visit(ESEQ n);
+	public Temp.Temp visit(NAME n);
+	public Temp.Temp visit(CONST n);
+	public Temp.Temp visit(CALL n);
 }
